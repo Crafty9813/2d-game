5ec8239a14b60a64417a64de260d25aeb19adaf1
Jonathan Li <jonathanli668899@gmail.com>
2024-10-02 12:29:54 -0400

    test

---
+++ src/characters/Player.java
@@ -0,0 +1,123 @@
+package characters;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+import main.KeyboardIO;
+
+public class Player extends Character{
+    
+    GamePanel gp;
+    KeyboardIO keyIO;
+
+    public Player(GamePanel gp, KeyboardIO keyIO) {
+        this.gp = gp;
+        this.keyIO = keyIO;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues() {
+        x = 100;
+        y = 100;
+        speed = 5;
+        direction = "down";
+    }
+    public void getPlayerImage() {
+        
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveUp1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveUp2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveDown1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveDown2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveLeft1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveLeft2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveRight1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/PlayerMoveRight2.png"));
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() { //gets called 60 times per sec
+
+        //makes sure the player doesn't move unless keys pressed
+        if (keyIO.upPressed == true || keyIO.downPressed == true || keyIO.leftPressed == true || keyIO.rightPressed == true) {
+            if (keyIO.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if (keyIO.downPressed) {
+                direction = "down";
+                y += speed;
+            } else if (keyIO.leftPressed) {
+                direction = "left";
+                x -= speed;
+            } else if (keyIO.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++; //every frame adds 1 to the counter
+            if (spriteCounter > 4) { //if spriteCounter hits 4 then change img (player img changes every 4 frames)
+                if (spriteNum == 1) { 
+                    spriteNum = 2;
+                }
+                else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+    }
+    }
+    public void draw(Graphics2D g2) {
+
+        //g2.setColor(Color.white);
+        //g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+        switch (direction) {
+        case "up":
+            if (spriteNum == 1) {
+                 image = up1;
+            }
+            if (spriteNum == 2) {
+                image = up2;
+            }
+            break;
+        case "down":
+            if (spriteNum == 1) {
+                image = down1;
+            }
+            if (spriteNum == 2) {
+                image = down2;
+            }
+            break;
+        case "left":
+            if (spriteNum == 1) {
+            image = left1;
+            }
+            if (spriteNum == 2) {
+                image = left2;
+            }
+            break;
+        case "right":
+            if (spriteNum == 1) {
+                image = right1;
+            }
+            if (spriteNum == 2) {
+                image = right2;
+            }
+            break;
+        }
+
+        //imageObserver is null bc we dont need it
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,127 @@
+package main;
+
+import javax.swing.JPanel;
+
+import characters.Player;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+public class GamePanel extends JPanel implements Runnable{
+    
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 2; //character will look like 48 x 48
+
+    public final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 20;
+    final int maxScreenRow = 16;
+    final int screenWidth = tileSize * maxScreenCol; //
+    final int screenHeight = tileSize * maxScreenRow; //px
+
+    int FPS = 60;
+
+    KeyboardIO keyIO = new KeyboardIO();
+    Thread gameThread;
+    Player player = new Player(this, keyIO);
+
+    //players initial pos
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.gray);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyIO);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    //THREAD.SLEEP METHOD
+    @Override
+   /* public void run() {
+        double drawInterval = 1000000000/FPS; //1 billion nano sec = 1 sec
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null) {
+         //System.out.println("game loop running");
+
+            update(); //updates info like character pos
+
+            repaint(); //draw screen with updated info
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long)remainingTime);
+                nextDrawTime += drawInterval;
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+       }
+    }
+    */
+
+    //DELTA METHOD
+    public void run() {
+        double drawInterval = 1000000000/FPS; //time per frame drawing
+        //since we want 60 fps we divide 1 sec (1000000000 nano sec) by 60 to get
+        //how much time needs to pass per frame in order to get 60 frames in 1 sec
+
+        double delta = 0; //time accumulated
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+             //find time elapsed and divide it by the drawInterval to get how many frames have passed
+            delta += (currentTime - lastTime) / drawInterval;
+        
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            //if at least 1 update frame happens then update and continue
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        player.draw(g2);
+        g2.dispose(); //saves memory by getting rid of any resources graphics is using
+    }
+}
